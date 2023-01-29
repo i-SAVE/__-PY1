82a5b1fc039f9eec988f839dcbdfd7706e45b8f3
i-SAVE <119124693+i-SAVE@users.noreply.github.com>
2023-01-30 00:25:38 +0300

    Update Class_LYB.py

--- Лабораторная работа 2/Class_LYB.py
+++ Лабораторная работа 2/Class_LYB.py
@@ -30,30 +30,25 @@ class Library:  # класс Library
         if len(self.books) == 0:
             return 1
         else:
-            id_res = self.books["id"]
+            id_res = self.books[-1].get("id")
             return id_res + 1
+
+    def get_index_by_book_id(self, id):
+        for i, val in enumerate(self.books):
+            if val["id"] == id:
+                return i
+            else:
+                continue
+        raise ValueError("Книга с запрашиваемым id не существует")
 
 
 # TODO написать класс Library
 
 if __name__ == '__main__':
-    """
     empty_library = Library()  # инициализируем пустую библиотеку
     print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
 
-    list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
-    ]
-    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    library_with_books = Library(books=BOOKS_DATABASE)  # инициализируем библиотеку с книгами
     print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
 
     print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
-    """
-
-    books = {
-        "id": 1,
-        "name": "test_name_1",
-        "pages": 200,
-    }
-    res = Library(books=books)
-    print(res.get_next_book_id())
